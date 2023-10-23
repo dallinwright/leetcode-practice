d919d17d7b0081a0bac256df0e84d6d40e10addd
Dallin Wright <dallinwright01@gmail.com>
2023-10-22 18:47:57 -0700

    redoing the entire algo from scratch

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ Example 3:
 Input: nums = [-1,-1]
 Output: [0,0]
 '''
+import copy
+from typing import List
 
-NUMBERS = [5, 2, 6, 1]
-EXPECTED = [2, 1, 1, 0]
+NUMBERS = [1, 2]
+EXPECTED = [0, 0]
 
 
 def count_smaller_n_squared(numbers):
@@ -59,66 +61,57 @@ def count_smaller_n_log_n(numbers):
     :param numbers: Array of numbers
     :return: Array of counts, where a +1 is the number of smaller elements to the right of nums[i]
     """
-    def merge_sort_with_count(number_list):
-        # Return 0 for empty list
-        if len(number_list) <= 1:
-            return number_list, []
 
-        # Chop in half to get o nlogn, and floor to an int to get index. We recursive chop it in half.
-        mid = len(number_list) // 2
+    def _merge_sort_with_count(number_list: List[int]) -> (List[int], List[int]):
+        list_size = len(number_list)
+
+        # Return 0 for empty list
+        if list_size <= 1:
+            return number_list, [0]
+
+        # Chop in half to get O(n * log(n)), and floor to an int to get a usable index. We recursive chop it in half.
+        mid = list_size // 2
         left_split = number_list[:mid]
         right_split = number_list[mid:]
 
-        left, remaining_count = merge_sort_with_count(left_split)
-        right, right_counts = merge_sort_with_count(right_split)
+        # This at the deepest call in the recursive call stack, will return a 2 element list, and a count of 0 if
+        # the list has 2 equal numbers, otherwise a count of 1. The 2 elements will be sorted via merge sort.
+        left, left_counts = _merge_sort_with_count(left_split)
+        right, right_counts = _merge_sort_with_count(right_split)
 
-        merged, merged_counts = [], []
         left_count, right_count = 0, 0
+        merged = []
+        merged_counts = []
 
-        while left_count < len(left) and right_count < len(right):
-            # If left element at count is > right element at count, merge sort and append
-            left_element = left[left_count]
-            right_element = right[right_count]
+        left_list_size = len(left)
+        right_list_size = len(right)
 
-            if left_element > right_element:
-                merged.append(right_element)
+        while left_count < left_list_size and right_count < right_list_size:
+            left_element: int = left[left_count]
+            right_element: int = right[right_count]
 
-                remaining_left = len(left) - left_count
-
-                if right_count:
-                    element = right_counts[right_count] + remaining_left
-                else:
-                    element = remaining_left
-
-                # print(f"appending to merge count: {element}")
-                merged_counts.append(element)
-
-                # print(f"merged_counts: {merged_counts}")
-                right_count += 1
-            else:
-                left_element = left[left_count]
-
-                # print(f"left element: {left_element}")
+            if right_element < left_element:
                 merged.append(left_element)
 
-                # print(f"Current Merged: {merged}")
-                remaining_right = len(right_counts) - right_count
+                # This is where we keep count of the smaller elements to the right of numbers[i]
+                current_smaller_than_count: int = left_counts[left_count]
+                new_smaller_than_count: int = current_smaller_than_count + 1
 
-                # print(f"remaining_right: {remaining_right}")
-                # print(f"Merged in right > left: {merged}")
-                # print(f"right_counts: {right_counts}, right_count: {right_count}")
-
-                element = right_counts[remaining_right]
-                merged_counts.append(element)
+                merged_counts.append(new_smaller_than_count)
 
                 left_count += 1
+            else:
+                merged.append(right_element)
 
-        merged += left[left_count:]
+                right_count += 1
+
         merged_counts += right_counts[right_count:]
+        merged += left[left_count:]
+        merged_with_counts = (merged, merged_counts)
 
-        return merged, merged_counts
+        return merged_with_counts
 
-    _, result = merge_sort_with_count(numbers)
+    _, result = _merge_sort_with_count(numbers)
     return result
 
 
@@ -128,8 +121,19 @@ def test_count_smaller_n_squared():
     assert results == EXPECTED
 
 
-def test_count_smaller_n_log_n():
-    results = count_smaller_n_log_n(NUMBERS)
+def test_small_list_already_ordered():
+    numbers = [1, 2]
+    expected = [0, 0]
 
-    assert results == EXPECTED
+    results = count_smaller_n_log_n(numbers)
 
+    assert results == expected
+
+
+def test_small_list_not_ordered():
+    numbers = [2, 1]
+    expected = [1, 0]
+
+    results = count_smaller_n_log_n(numbers)
+
+    assert results == expected
