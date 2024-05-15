d93876666f3c46650730b676680bc092554996b9
Dallin Wright <dallinwright@tradecrit.com>
2024-05-15 12:58:20 -0700

    working on samples

--- src/config/app_logger.py
+++ src/config/app_logger.py
@@ -1,28 +1,5 @@
-import logging
-from logging import config
+import sys
 
-log_config = {
-    "version": 1,
-    "root": {
-        "handlers": ["console"],
-        "level": 'DEBUG'
-    },
-    "handlers": {
-        "console": {
-            "formatter": "std_out",
-            "class": "logging.StreamHandler",
-            "level": "INFO"
-        }
-    },
-    "formatters": {
-        "std_out": {
-            "format": "%(asctime)s : %(levelname)s : %(module)s : %(funcName)s : %(lineno)d : (Process Details : (%("
-                      "process)d, %(processName)s), Thread Details : (%(thread)d, %(threadName)s))\nLog : %(message)s",
-            "datefmt": "%d-%m-%Y %I:%M:%S"
-        }
-    },
-}
+from loguru import logger
 
-config.dictConfig(log_config)
-
-logger = logging.getLogger(__name__)
+logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
