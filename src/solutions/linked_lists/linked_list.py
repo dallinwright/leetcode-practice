1663528761ac69884670d572de7386f895e651ee
Dallin Wright <dallinwright01@gmail.com>
2024-07-01 12:49:03 -0700

    working on auth menu in navbar

--- src/solutions/linked_lists/linked_list.py
+++ src/solutions/linked_lists/linked_list.py
@@ -37,43 +37,65 @@ class LinkedList:
             new_node.prev = current_tail
             self.tail = new_node
 
-    def insert_at_position(self, value, position):
+    def insert_next(self, existing_node: Node, value: int):
         new_node = Node(value)
+
+        if existing_node is None:
+            raise ValueError("Unable to proceed with none node")
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+
+        if existing_node.next is None:
+            existing_node.next = new_node
+            new_node.prev = existing_node
+            self.tail = new_node
+        else:
+            existing_node.next = new_node
+            new_node.prev = existing_node
+
+    def insert_previous(self, existing_node: Node, value: int):
+        new_node = Node(value)
+
+        if existing_node is None:
+            raise ValueError("Unable to proceed with none node")
 
         if self.head is None:
             self.head = new_node
+        if self.tail is None:
             self.tail = new_node
+
+        if existing_node.prev is None:
+            existing_node.prev = new_node
+            new_node.next = existing_node
+            self.head = new_node
         else:
-            current_node = self.head
-            current_position = 0
+            current_previous = existing_node.prev
 
-            while current_node is not None:
-                if current_position == position:
-                    new_node.next = current_node
-                    new_node.prev = current_node.prev
-                    current_node.prev.next = new_node
-                    current_node.prev = new_node
-                    break
+            current_previous.next = new_node
+            existing_node.prev = new_node
 
-                current_position += 1
-                current_node = current_node.next
+            new_node.prev = current_previous
+            new_node.next = existing_node
 
     def list_snapshot(self):
-        current_node = self.head
+        current_node: Node = self.head
 
         if current_node is None:
             print("List is empty")
             return
 
-        message = f"{current_node.value}"
-        current_node = current_node.next
+        current_value: int = current_node.value
+        message: str = f"{current_value}"
+        current_node: Node = current_node.next
 
         while current_node is not None:
             message = f"{message} -> {current_node.value}"
             current_node = current_node.next
 
         return message
-
 
 
 def test_linked_list():
@@ -96,8 +118,15 @@ def test_linked_list():
 
     assert linked_list.list_snapshot() == "5 -> 4 -> 3 -> 2 -> 1 -> 6 -> 0 -> 0 -> 3 -> 1"
 
-    linked_list.insert_at_position(0, 1)
-    linked_list.insert_at_position(12, 5)
-    linked_list.insert_at_position(13, 3)
+    short_list = LinkedList()
+    short_list.insert_start(1)
 
-    assert linked_list.list_snapshot() == "5 -> 0 -> 4 -> 13 -> 3 -> 2 -> 12 -> 1 -> 6 -> 0 -> 0 -> 3 -> 1"
+    assert short_list.list_snapshot() == "1"
+
+    short_list.insert_next(short_list.head, 3)
+
+    assert short_list.list_snapshot() == "1 -> 3"
+
+    short_list.insert_previous(short_list.tail, 2)
+
+    assert short_list.list_snapshot() == "1 -> 2 -> 3"
--- src/solutions/linked_lists/linked_list_sorted_merge/main.py
+++ src/solutions/linked_lists/linked_list_sorted_merge/main.py
@@ -1,45 +1,52 @@
+from loguru import logger
+
 from src.solutions.linked_lists.linked_list import LinkedList, Node
 
 
-def merge_lists(first: LinkedList, second: LinkedList):
-    first_node: Node = first.head
-    second_node: Node = second.head
-    new_list = LinkedList()
+def merge_lists(first_list: LinkedList, second_list: LinkedList):
+    first_node: Node = first_list.head
+    second_node: Node = second_list.head
 
-    while first_node is not None or second_node is not None:
-        # Gets tricky real fast. If we have no more nodes in a list, since they are sorted we insert the remaining
-        # values in the other list.
-        if first_node is None:
-            new_list.insert_end(second_node.value)
-            second_list_node = second_node.next
-        elif second_node is None:
-            new_list.insert_end(first_node.value)
-            first_list_node = first_node.next
+    while first_node is not None and second_node is not None:
+        first_value = first_node.value
+        second_value = second_node.value
+
+        if second_value < first_value:
+            first_list.insert_previous(first_node, second_value)
+            second_node = second_node.next
         else:
-            # We have two values to insert, but no guarantee both values are greater than the current tail value.
-            # We need to then go backwards  and then insert.
-            current_tail: Node = new_list.tail
+            if first_node.next is None and second_node is not None:
+                while second_node is not None:
+                    first_list.insert_end(second_node.value)
+                    second_node = second_node.next
+                return first_list
 
-    return new_list
+            first_node = first_node.next
+
+    return first_list
 
 
 def test_merge_lists():
+    logger.info("\n")
+    logger.info("Merging")
     first_list = LinkedList()
 
     first_list.insert_end(1)
     first_list.insert_end(3)
-    first_list.insert_end(5)
     first_list.insert_end(9)
 
-    assert first_list.print_list() == "1 -> 3 -> 5 -> 9"
+    assert first_list.list_snapshot() == "1 -> 3 -> 9"
 
     second_list = LinkedList()
-    second_list.insert_end(1)
+    second_list.insert_end(2)
     second_list.insert_end(4)
     second_list.insert_end(10)
 
-    first_list.print_list()
-    second_list.print_list()
+    assert second_list.list_snapshot() == "2 -> 4 -> 10"
+
+    logger.info(first_list.list_snapshot())
+    logger.info(second_list.list_snapshot())
 
     summed_result = merge_lists(first_list, second_list)
-    summed_result.print_list()
+
+    logger.info(summed_result.list_snapshot())
